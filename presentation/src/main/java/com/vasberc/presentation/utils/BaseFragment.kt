ba45e2e6d3049dd717f7f8516df69a4d8abe2242
vasberc <52175508+vasberc@users.noreply.github.com>
2024-03-18 23:11:14 +0200

    Create movie detail view layout (#11)

--- presentation/src/main/java/com/vasberc/presentation/utils/BaseFragment.kt
+++ presentation/src/main/java/com/vasberc/presentation/utils/BaseFragment.kt
@@ -3,10 +3,15 @@ package com.vasberc.presentation.utils
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract  class BaseFragment<BINDING: ViewBinding>(@LayoutRes layout: Int): Fragment(layout) {
+
+    @get:LayoutRes
+    protected open val supportActionBar: Int? = null
+    protected open val homeAsUp: Boolean = false
 
     private var _binding: BINDING? = null
     val binding get() = _binding!!
@@ -14,8 +19,18 @@ abstract  class BaseFragment<BINDING: ViewBinding>(@LayoutRes layout: Int): Frag
     protected abstract fun createViewBinding(view: View): BINDING
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         _binding = createViewBinding(view)
+        super.onViewCreated(view, savedInstanceState)
+        if (supportActionBar != null) {
+            (activity as? AppCompatActivity)?.setSupportActionBar(view.findViewById(supportActionBar!!))
+        }
+        val supportActionBar = (activity as? AppCompatActivity)?.supportActionBar
+
+        if (homeAsUp) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        } else {
+            supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        }
     }
 
     override fun onDestroyView() {
